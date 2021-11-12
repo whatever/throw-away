0e156c1666e1d52083f82fe93c645feb0ba3b725
Matt 3 <not.mattowen@gmail.com>
2021-11-11 20:37:36 -0500

    :snake: include a bunch files of files... don't know what's really necessary

---
+++ iOS/Shared/Assets.xcassets/dist/bundle.dataset/bundle.js
@@ -0,0 +1,2 @@
+var el = document.getElementById("background");
+el.innerHTML = "nothing really falls short";
---
+++ iOS/Shared/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by matt on 11/11/21.
+//
+
+import SwiftUI
+import WebKit
+
+// WebKit Web View (Full Screen)
+// Identical to WKWebView - but ignore
+class WKWebViewFullScreen: WKWebView {
+    override var safeAreaInsets: UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+}
+
+// WebView
+// WebView usable as part of a UI. Ignores Safe area.
+struct WebView : UIViewRepresentable {
+    let url: URL
+    func makeUIView(context: Context) -> WKWebView {
+        WKWebViewFullScreen()
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let url = Bundle.main.url(
+            forResource: "index",
+            withExtension: "html",
+            subdirectory: "dist"
+        )!
+        uiView.loadFileURL(url, allowingReadAccessTo: url)
+    }
+}
+
+struct ContentView: View {
+    var webview = WebView(url: URL(string: "file://index.html")!)
+    var body: some View {
+        
+        VStack {
+            Text("what?").padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(webview)
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        .previewInterfaceOrientation(.portrait)
+    }
+}
