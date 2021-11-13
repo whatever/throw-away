994fe9e94d4e4b0e8150462c1d9901b6d9d162af
Matt 3 <not.mattowen@gmail.com>
2021-11-13 14:02:35 -0500

    :leaves: leave after 5 seconds

--- iOS/Shared/LFGApp.swift
+++ iOS/Shared/LFGApp.swift
@@ -7,11 +7,19 @@
 
 import SwiftUI
 
+
+func lezgo() {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+        exit(0)
+    }
+}
+
 @main
 struct LFGApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear(perform: lezgo)
         }
     }
 }
